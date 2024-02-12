26b3bcefa04ba8a8ed044ea0ffec3c77d3d1ca87
arianit <arianitlikaj46@gmail.com>
2024-02-12 16:02:11 +0100

    first commit

---
+++ src/data.js
@@ -0,0 +1,20 @@
+ export default [
+    {
+        id:1,
+        name: 'Betie Yates',
+        age: 35,
+        image:'https://cdn.britannica.com/89/152989-050-DDF277EA/Johnny-Depp-2011.jpg'
+    },
+    {
+        id:2,
+        name: 'Stylver Stallone',
+        age: 64,
+        image:'https://cdn.britannica.com/26/157026-050-08ED6418/Sylvester-Stallone-1998.jpg'
+    },
+    {
+        id:3,
+        name: 'Van Damme',
+        age: 67,
+        image:'https://br.web.img3.acsta.net/c_310_420/pictures/15/05/11/23/23/500100.jpg'
+    }
+ ]
